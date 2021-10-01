175a192ff9c7006a8dedc0b936caa9783a937ca6
marlloncsoares <marlonmcs@hotmail.com>
2021-10-01 01:07:55 -0300

    feat: Atualiza estudos sobre o VueJS.

---
+++ guide-documentation/src/views/DeclarativeRendering.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+      Counter: {{ counter }}
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'DeclarativeRendering',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+</script>
+
+<style>
+main {
+  /* background-color: tomato; */
+  max-width: 960px;
+  height: 500px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
